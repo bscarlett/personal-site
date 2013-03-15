aa505d990fc9b5ff946e67cc01d259fcd46f62bf
Brad Scarlett <brad.scarlett@gmail.com>
2013-03-15 23:59:35 +1100

    More tests, but tired now.

--- tests/testPersonalSite.py
+++ tests/testPersonalSite.py
@@ -28,5 +28,56 @@ class testPersonalSite(unittest.TestCase):
         assert e.find(".//div[@id='content']/p").text == 'Test Content'
         assert e.find(".//ul[@id='navigation']//a[@href='/']").text == 'Test Title'
 
+    def testOtherRoute(self):
+        content = self.db.Content()
+        content._id = u'other'
+        content.content = u'other content'
+        content.save()
+
+        rsp = self.app.get('/other')
+        assert rsp.status_code == 200
+
+        e = etree.HTML(rsp.data)
+        assert e.find(".//div[@id='content']/p").text == 'other content'
+
+    def test404(self):
+        content = self.db.Content()
+        self._id = u'there_is_a_route_for_this'
+        content.save()
+
+        rsp = self.app.get('/there_is_no_route_for_this')
+        assert rsp.status_code == 404
+
+    def testContentIsMarkdown(self):
+        content = self.db.Content()
+        content._id = u'route'
+        content.content = u'#should be a heading 1#'
+        content.save()
+
+        rsp = self.app.get('/route')
+        assert rsp.status_code == 200
+
+        e = etree.HTML(rsp.data)
+        assert e.find(".//div[@id='content']/h1").text == 'should be a heading 1'
+
+    def testBrowsing(self):
+        content1 = self.db.Content()
+        content1._id = u'/'
+        content1.title = u'home'
+        content1.content = u'thing'
+        content1.show_in_navigation = True
+        content1.save()
+
+        content2 = self.db.Content()
+        content2._id = u'thing'
+        content2.title = u'thing'
+        content2.title = u'thing2'
+        content2.show_in_navigation = True
+        content2.save()
+
+        rsp = self.app.get('/')
+        assert rsp.status_code == 200
+        #to be continued..
+
     def tearDown(self):
         self.db.Content.delete()
